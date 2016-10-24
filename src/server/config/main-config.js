15f9a26bd018c60592dd5c4bb4ac6c5d6674c55b
johnmberger <johnmartinberger@gmail.com>
2016-10-24 14:38:15 -0600

    changed CORS settings

--- src/server/config/main-config.js
+++ src/server/config/main-config.js
@@ -25,8 +25,8 @@
     // *** cross domain requests *** //
     const allowCrossDomain = function(req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
-      // res.header('Access-Control-Allow-Methods', 'GET ,PUT, POST, DELETE');
-      // res.header('Access-Control-Allow-Headers', 'Content-Type');
+      res.header('Access-Control-Allow-Methods', 'GET ,PUT, POST, DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
       next();
     };
 
