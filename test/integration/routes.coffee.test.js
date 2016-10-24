f4bd634450f2cee5f1ff1aea89f72b541d63a206
johnmberger <johnmartinberger@gmail.com>
2016-10-24 10:40:42 -0600

    set up integration tests

--- test/integration/routes.coffee.test.js
+++ test/integration/routes.coffee.test.js
@@ -5,14 +5,16 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-const knex = require('../../src/server/db/connection');
 const server = require('../../src/server/app');
+const knex = require('../../src/server/db/connection');
 
 describe('routes : coffee', () => {
 
-  beforeEach((done) => { done(); });
-
-  afterEach((done) => { done(); });
+  beforeEach(() => {
+    return knex.migrate.rollback()
+    .then(() => { return knex.migrate.latest(); })
+    .then(() => { return knex.seed.run(); });
+  });
 
   describe('GET /coffee', () => {
     it('should render a success', (done) => {
@@ -25,9 +27,31 @@ describe('routes : coffee', () => {
           res.status.should.eql(200);
           res.type.should.eql('application/json');
           res.body.should.contain.keys('status', 'data');
-          res.body.status.should.equal('success');
-          res.body.data.should.be('array');
-          res.body.data.id.should.eql(1);
+          res.body.status.should.eql('success');
+          res.body.data.should.be.a('array');
+          res.body.data.length.should.eql(10);
+          res.body.data[0].id.should.eql(coffee.id);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('GET /coffee/:id', () => {
+    it('should render a success', (done) => {
+      chai.request(server)
+      .get('/coffee/2')
+      .end((err, res) => {
+        return knex('coffee').where('id', 2).first()
+        .then((coffee) => {
+          res.redirects.length.should.equal(0);
+          res.status.should.eql(200);
+          res.type.should.eql('application/json');
+          res.body.should.contain.keys('status', 'data');
+          res.body.status.should.eql('success');
+          res.body.data.should.be.a('object');
+          res.body.data.id.should.eql(coffee.id);
+          res.body.data.name.should.eql(coffee.name);
           done();
         });
       });
